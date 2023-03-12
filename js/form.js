3b07fbb75df60769313d92a8d6e27c3f0c416be9
AlexSearha <contact@mytechcompanion.fr>
2023-03-12 15:31:39 +0100

    HTML & CSS bureau OK

--- js/form.js
+++ js/form.js
@@ -21,24 +21,77 @@ const app = {
     },
   
     showNameOnCard() {
-      const nameCardElement =document.getElementById('nameCard');
+      const nameCardElement = document.getElementById('nameCard');
       nameCardElement.textContent = "YOUR NAME HERE";
   
       const cardHolderNameElement = document.getElementById('cardholderName');
       cardHolderNameElement.addEventListener('input', (event) => {
-  
+        if(cardHolderNameElement.value === ''){
+          nameCardElement.textContent = "YOUR NAME HERE";
+        }else{
+          nameCardElement.textContent = cardHolderNameElement.value;
+        }
       }); 
     },
-  
-    changeName(event) {
-      event
+    
+    showCardValidity() {
+      const expirationCardMonthElement = document.getElementById('expirationCardMonth');
+      const expirationCardYearElement = document.getElementById('expirationCardYear');
+
+      const expMonthElement = document.getElementById('expMonth');
+      expMonthElement.addEventListener('input', () => {
+        if(expMonthElement.value === ''){
+          expirationCardMonthElement.textContent = "XX";
+        }else{
+          expirationCardMonthElement.textContent = expMonthElement.value;
+        }
+      });
+
+      const expYearElement = document.getElementById('expYear');
+      expYearElement.addEventListener('input', () => {
+        if(expYearElement.value === ''){
+          expirationCardYearElement.textContent = "XX";
+        }else{
+          expirationCardYearElement.textContent = expYearElement.value;
+        }
+      });
+
     },
+
+    cvcRectoCardNumbers() {
+      const numbersVersoCardElement = document.getElementById('numbersVersoCard');
+
+      const cvcCodeElement = document.getElementById('cvcCode');
+      cvcCodeElement.addEventListener('input', () => {
+        if(cvcCodeElement.value === ''){
+          numbersVersoCardElement.textContent = "XXX";
+        }else{
+          numbersVersoCardElement.textContent = cvcCodeElement.value;
+        }
+      });
+    },
+
+
+    cardRectoNumbers() {
+      const cardNumbersElement = document.getElementById('cardNumbers');
+
+      const cardNumberInputElement = document.getElementById('cardNumberInput');
+      cardNumberInputElement.addEventListener('input', () => {
+        cardNumbersElement.textContent = cardNumberInputElement.value;
+      });
+
+    },
+
+
       
     init() {
       app.generateCircles('one');
       app.generateCircles('two');
       app.inputSelected();
       app.showNameOnCard();
+      app.showCardValidity();
+      app.cvcRectoCardNumbers();
+      app.cardRectoNumbers();
     },
   
   }
